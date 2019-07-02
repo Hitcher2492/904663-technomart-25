16742f201be69c12361e938de282b4041d3652d4
Hitcher2492 <fan2492@mail.ru>
2019-07-03 00:17:17 +0300

    Update catalog.js

--- js/catalog.js
+++ js/catalog.js
@@ -1,36 +1,42 @@
-var overlay = document.querySelector(".overlay");
+// ------ПЕРЕМЕННЫЕ ДЛЯ МОДАЛЬНОГО ОКНА MODAL-CART------
 
-var linkCatalog = document.querySelector(".item-actions-buy");
+var overlayModalCart = document.querySelector(".overlay-cart");
+
+var linkCatalog = document.querySelectorAll(".item-actions-buy");
 
 var popupCatalog = document.querySelector(".modal-cart");
-var closeCatalog = popupCatalog.querySelector(".modal-close");
-var closeBtnCatalog = popupCatalog.querySelector(".btn-modal-close");
+var closeCatalog = document.querySelector(".modal-cart .modal-close");
+var closeBtnCatalog = document.querySelector(".btn-modal-close"); 
 
-linkCatalog.addEventListener("click", function(evt) {
-	evt.preventDefault();
-	popupCatalog.classList.add("modal-animation-catalog");
-	overlay.style.display = "flex";
-	popupCatalog.style.display = "block";
-});
+// ------МОДАЛЬНОЕ ОКНО MODAL-CART------
 
+// ------Открытие окна------
+for (let i = 0; i < linkCatalog.length; i++) {
+	linkCatalog[i].addEventListener("click", function(evt) {
+		evt.preventDefault();
+		popupCatalog.classList.add("modal-animation-catalog");
+		overlayModalCart.style.display = "flex";
+	});
+}
+
+// ------Закрытие окна при нажатии на крестик------
 closeCatalog.addEventListener("click", function(evt) {
 	evt.preventDefault();
-	popupCatalog.style.display = "none";
-	overlay.style.display = "none";
+	overlayModalCart.style.display = "none";
 });
 
+// ------Закрытие окна при нажатии на кнопку Продолжить покупки------
 closeBtnCatalog.addEventListener("click", function(evt) {
 	evt.preventDefault();
-	popupCatalog.style.display = "none";
-	overlay.style.display = "none";
+	overlayModalCart.style.display = "none";
 });
 
+// ------Закрытие окна при нажатии на Esc------
 window.addEventListener("keydown", function(evt) {
 	if (evt.keyCode === 27) {
-		if (overlay.style.display = "flex") {
+		if (overlayModalCart.style.display = "flex") {
 			evt.preventDefault();
-			popupCatalog.style.display = "none";
-			overlay.style.display = "none";
+			overlayModalCart.style.display = "none";
 		}
 	}
 });
